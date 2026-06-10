eb21766b8b52efb855801d764c87f04b6efbeaf9
Pritom Chowdhury <coderpritom03@gmail.com>
2026-06-10 22:11:15 +0600

    fix: Remove SameSite attribute from JWT cookie for compatibility

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -8,9 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, 
     httpOnly: true, 
-    sameSite: "lax", 
     secure: process.env.NODE_ENV !== 'development' ,
-    domain: '.onrender.com' 
+
     });
 
     return token;
